870b63995c7196359b67f9192382a26b6da62324
Risher Randall <randallr18@mail.wlu.edu>
2018-05-31 17:21:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+    
+  counter = 2
+
+  while counter < integer
+    if integer % counter == 0
+      return false
+   else
+     counter += 1
+   end
+ end
+ 
+ return true
+ end
+     
