4e17ec416ba047e4d81a3bf1db44881b2d8a8a86
Risher Randall <randallr18@mail.wlu.edu>
2018-05-31 17:17:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  counter = 2
+  while counter < integer
+    if integer % counter == 0
+      return false
+   else
+     counter += 1
+   end
+ end
+ return true
+ end
+     
